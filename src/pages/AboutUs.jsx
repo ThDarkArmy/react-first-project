1ae7aecc9e1044db15e83f8ae89e129752317cd3
Prashant <thepenseur1729@gmail.com>
2024-09-20 18:57:48 +0530

    changes in about us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div>
+        <h3>About us</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas doloribus iusto nobis possimus labore? Quo dolores, impedit modi deserunt voluptatum cumque obcaecati consequatur necessitatibus incidunt beatae animi repellendus illum provident?</p>
+    </div>
   )
 }
 
